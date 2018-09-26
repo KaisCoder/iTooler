e2886a9af0bc57775262c223a740d60bde0f9f53
Adair <616653486@qq.com>
2018-09-26 17:13:24 +0800

    添加kit

---
+++ iTooler-Kit/src/main/java/cn/adair/itooler/iAbc.java
@@ -0,0 +1,14 @@
+package cn.adair.itooler;
+
+/**
+ * cn.adair.itooler
+ * Created by Administrator on 2018/9/26/026.
+ * slight negligence may lead to great disaster~
+ */
+public class iAbc {
+
+    public String aVoid() {
+        return "iAbc.aVoid()";
+    }
+
+}
